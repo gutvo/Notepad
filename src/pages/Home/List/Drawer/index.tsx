94fc7f6e7ff1e3e0db746bda59d1631e44f3aa77
Gustavo <gutvo10@gmail.com>
2026-02-15 01:24:17 -0300

    chore: replace base drawer component

--- src/pages/Home/List/Drawer/index.tsx
+++ src/pages/Home/List/Drawer/index.tsx
@@ -1,7 +1,7 @@
-import BaseDrawer from "@Components/BaseDrawer";
 import BaseIcon from "@Components/BaseIcon";
 import BaseButton from "@Components/bases/Button";
 import BaseDivider from "@Components/bases/Divider";
+import BaseDrawer from "@Components/bases/Drawer";
 import BaseTypography from "@Components/BaseTypography";
 import { useCurrentModal } from "@Hooks/useCurrentModal";
 import useOpenModal from "@Hooks/useOpenModal";
