6cbd61354f2193ca2acca0ce1808e1173eda57b2
Gustavo <gutvo10@gmail.com>
2026-03-03 19:45:03 -0300

    style: add card color

--- src/theme/backgroundTheme.ts
+++ src/theme/backgroundTheme.ts
@@ -2,11 +2,13 @@ import defaultColors from "./defaultColors";
 
 const lightBackground = {
   body: defaultColors.grey[50],
+  card: defaultColors.grey[100],
   border: defaultColors.grey[500],
 };
 
 const darkBackground = {
   body: defaultColors.grey[900],
+  card: defaultColors.grey[800],
   border: defaultColors.grey[500],
 };
 
